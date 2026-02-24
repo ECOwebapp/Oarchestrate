94a0b6fee487a0b3cf7805bc232d5cfafc8af69f
shadowhexer <shadowhexer@yandex.com>
2026-02-24 10:55:27 +0800

    Added router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup vapor>
+
+</script>
+
+<template>
+    <nav class="flex flex-col justify-evenly">
+        <ul>Dashboard</ul>
+        <ul>Tasks</ul>
+        <ul>Design</ul>
+        <ul>Organisation</ul>
+        <ul>Analytics</ul>
+        <ul>Profile</ul>
+        <ul>Logout</ul>
+    </nav>
+</template>
