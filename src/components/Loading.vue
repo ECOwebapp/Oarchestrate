ed65c3096eaf56595d5609fc6af6ba9583c1f416
shadowhexer <shadowhexer@yandex.com>
2026-03-28 17:31:20 +0800

    Reduced the gap in between elements

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -12,7 +12,7 @@ defineProps(['message'])
       </svg>
       <span class="text-sm text-gray-500 tracking-wide">Loading…</span>
     </div> -->
-    <div class="flex flex-col items-center justify-center gap-5 w-full h-screen overflow-hidden bg-white">
+    <div class="flex flex-col items-center justify-center gap-3 w-full h-screen overflow-hidden bg-white">
       <div class="w-25 h-40 motion-safe:animate-bounce bg-[url('/images/csu_seal.png')] bg-cover bg-center no-repeat" />
 
       <p class="animate-pulse text-gray-500/70 text-sm">{{ message }}</p>
